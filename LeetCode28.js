fcfe7bbb8ccfe2ecb1cd1fdca9ae42894c7e9b68
mokhc <mokhonchuen@yahoo.com>
2024-09-01 11:37:11 +0800

    Add files via upload

---
+++ LeetCode28.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+// Test - LC28
+// @Author - mokhc
+// @Date - 01/09/24
+// Trivial - What is the function of a key in a Map?
+/*
+var strStr = function (haystack, needle) {
+    // return the index of the first occurrence of the matched string
+    return haystack.indexOf(needle);
+};
+*/
+
+var strStr = function (haystack, needle) {
+    // return the index of the first occurrence of the matched string
+    return haystack.search(needle)
+};
+
